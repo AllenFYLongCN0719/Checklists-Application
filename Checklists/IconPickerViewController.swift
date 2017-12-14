26a2d176b99194dee5bf7f2cde56381d80de6b5e
Long Fu Yu <33461058+AllenFYLong@users.noreply.github.com>
2017-12-14 21:39:30 +0800

    12/14/2017 Updated
    Added IconPickerViewController.swift to Project.

---
+++ Checklists/IconPickerViewController.swift
@@ -0,0 +1,39 @@
+//
+//  IconPickerViewController.swift
+//  Checklists
+//
+//  Created by 龙富宇 on 2017/12/14.
+//  Copyright © 2017年 AllenLong. All rights reserved.
+//
+
+import UIKit
+
+    protocol IconPickerViewControllerDelegate: class {
+        func IconPicker(_ picker: IconPickerViewController, didPick iconName: String)
+    }
+    
+    class IconPickerViewController: UITableViewController {
+        weak var delegate: IconPickerViewControllerDelegate?
+        
+        let icons = ["No Icon",
+                     "cake"]
+        
+        override func tableView (_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return icons.count
+            //返回数组中的对象数量
+        }
+        
+        override func tableView (_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            //这里得到一个table view cell并且给它一个文本和图片。
+            let cell = tableView.dequeueReusableCell(withIdentifier: "IconCell", for: indexPath)
+            let iconName = icons[indexPath.row]
+            cell.textLabel!.text = iconName
+            cell.imageView!.image = UIImage(named: iconName)
+            
+            return cell
+        }
+        
+    }
+    
+
+
